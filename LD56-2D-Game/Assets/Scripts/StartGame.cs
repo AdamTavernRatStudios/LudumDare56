62225a2bff55d0198d5077b3f6ff20f0a8bf230e
k8 <katelyn.tavernratstudios@gmail.com>
2024-10-06 13:58:55 -0700

    new button on instructions

--- LD56-2D-Game/Assets/Scripts/StartGame.cs
+++ LD56-2D-Game/Assets/Scripts/StartGame.cs
@@ -10,4 +10,8 @@ public class StartGame : MonoBehaviour
     {
         LevelLoader.Instance.LoadLevel("Instructions");
     }
+    public void LoadMainMenu()
+    {
+        LevelLoader.Instance.LoadLevel(levelName: "TitleScreen");
+    }
 }
