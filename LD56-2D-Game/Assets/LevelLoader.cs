50561c1a059e353e86a834ccfc7cd33358ca5135
AdamTavernRatStudios <adam.tavernratstudios@gmail.com>
2024-10-06 12:10:06 -0700

    Transition UI

---
+++ LD56-2D-Game/Assets/LevelLoader.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public static LevelLoader Instance { get; private set; }
+    public Animator anim;
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
+    public void LoadLevel(string levelName)
+    {
+        StartCoroutine(LoadLevelRoutine(levelName));
+    }
+
+    bool Loading = false;
+    IEnumerator LoadLevelRoutine(string levelName)
+    {
+        if (Loading) yield break;
+        Loading = true;
+        anim.SetTrigger("Enter");
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(levelName);
+        anim.SetTrigger("Exit");
+        yield return new WaitForSeconds(1f);
+        Loading = false;
+    }
+}
--- LD56-2D-Game/Assets/Scripts/StartGame.cs
+++ LD56-2D-Game/Assets/Scripts/StartGame.cs
@@ -4,10 +4,10 @@ public class StartGame : MonoBehaviour
 {
     public void LoadGame()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("Game");
+        LevelLoader.Instance.LoadLevel("Game");
     }
     public void LoadInstructions()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("Instructions");
+        LevelLoader.Instance.LoadLevel("Instructions");
     }
 }
