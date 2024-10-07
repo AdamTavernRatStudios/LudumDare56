22046aa727c62429dc35c0df47530b590afd1fc4
JackZeli <Jackzeli@umich.edu>
2024-10-06 18:20:52 -0700

    Fixed bug where trampoline was not working

--- LD56-2D-Game/Assets/Trampoline.cs
+++ LD56-2D-Game/Assets/Trampoline.cs
@@ -13,13 +13,19 @@ public class Trampoline : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Debug.Log("oy3");
         if (collision.gameObject.layer == LayerMask.NameToLayer("Flea"))
         {
-            if (collision.relativeVelocity.y < 0f && collision.rigidbody.velocity.y == 0f)
+            Debug.Log("oy2");
+            Debug.Log(collision.relativeVelocity.y);
+            Debug.Log(collision.rigidbody.velocity.y);
+            if (collision.relativeVelocity.y < 0f && collision.rigidbody.velocity.y < 0.1f && collision.rigidbody.velocity.y > -0.1f)
             {
                 var f = collision.gameObject.GetComponent<Flea>();
-                if(f != null)
+                Debug.Log("oy1");
+                if (f != null)
                 {
+                    Debug.Log("oy");
                     ScoreManager.AddTrick(f, ScoreManager.TrickType.TrampolineBounce);
                     f.rb.velocity = new Vector2(f.rb.velocity.x, BounceHeight);
                     anim.SetTrigger("Bounce");
