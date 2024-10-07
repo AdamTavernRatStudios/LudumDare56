4435007de33a49e1ea7f028a81283786f108000e
AdamTavernRatStudios <adam.tavernratstudios@gmail.com>
2024-10-07 08:44:41 -0700

    Button fixes and things

---
+++ LD56-2D-Game/Assets/EventSystemCustom.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventSystemCustom : MonoBehaviour
+{
+    public static EventSystemCustom Instance { get; private set; }
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            transform.parent = null;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
