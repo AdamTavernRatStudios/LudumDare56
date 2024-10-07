2d11ea47a08af988a92ee1bf9e4f25654a9b84d2
AdamTavernRatStudios <adam.tavernratstudios@gmail.com>
2024-10-07 08:31:31 -0700

    ShopIcons

--- LD56-2D-Game/Assets/Scripts/FleaMarketButton.cs
+++ LD56-2D-Game/Assets/Scripts/FleaMarketButton.cs
@@ -12,12 +12,19 @@ public class FleaMarketButton : MonoBehaviour
     public Image image;
     public Image background;
 
+    Button button;
     private void Start()
     {
+        button = GetComponent<Button>();
         MainText.text = objectData.ObjectName + "<size=50%>" + '\n' + objectData.Description;
         image.sprite = objectData.sprite;
         cost.text = objectData.Cost.ToString();
         background.color = Utils.HexToColor(objectData.TentColor);
+    }
+
+    private void Update()
+    {
+        button.interactable = objectData.Cost <= ScoreManager.Instance.TotalMoney;
     }
 
     public void SetAsCurrentObject()
