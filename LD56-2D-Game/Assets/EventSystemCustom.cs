764bf6f4f214f8836e7a86dfdfcdd4866dc18470
AdamTavernRatStudios <adam.tavernratstudios@gmail.com>
2024-10-07 09:00:29 -0700

    More button fixes

--- LD56-2D-Game/Assets/EventSystemCustom.cs
+++ LD56-2D-Game/Assets/EventSystemCustom.cs
@@ -1,11 +1,13 @@
+using Rewired.Integration.UnityUI;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EventSystemCustom : MonoBehaviour
 {
     public static EventSystemCustom Instance { get; private set; }
-
+    RewiredEventSystem eventSystem;
     void Awake()
     {
         if (Instance == null)
@@ -23,12 +25,16 @@ public class EventSystemCustom : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        eventSystem = GetComponent<RewiredEventSystem>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(eventSystem.currentSelectedGameObject == null)
+        {
+            var firstSelectable = FindAnyObjectByType<Selectable>();
+            eventSystem.SetSelectedGameObject(firstSelectable.gameObject);
+        }
     }
 }
--- LD56-2D-Game/Assets/MatchWorldObjectPosition.cs
+++ LD56-2D-Game/Assets/MatchWorldObjectPosition.cs
@@ -7,7 +7,7 @@ public class MatchWorldObjectPosition : MonoBehaviour
     public Camera mainCamera => Camera.main;        // The camera used to render the world (usually the main camera)
     RectTransform uiElement;  // The UI element you want to position (must be in a Canvas)
     public Transform worldObject;    // The 3D object in the world you want to track
-
+    public Vector3 AdditionalOffset = Vector3.zero;
 
     private void Start()
     {
@@ -33,7 +33,7 @@ public class MatchWorldObjectPosition : MonoBehaviour
         if (screenPos.z > 0)
         {
             // Convert screen space to UI canvas space and set the position
-            uiElement.position = screenPos;
+            uiElement.position = screenPos + AdditionalOffset;
         }
         else
         {
